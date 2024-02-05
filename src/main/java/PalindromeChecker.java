0a30196eca3b121b4032812a041cce495760f011
SheaOdoherty <143014763+SheaOdoherty@users.noreply.github.com>
2024-02-05 10:04:48 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -22,17 +22,6 @@ public void tester()
         System.out.println("An error occurred.");
         e.printStackTrace();
     }
- public void setup() {
-    String lines[] = loadStrings("palindromes.txt");
-    System.out.println("there are " + lines.length + " lines");
-    for (int i = 0; i < lines.length; i++) {
-        if (palindrome(lines[i])) {
-            System.out.println(lines[i] + " IS a palindrome.");
-        } else {
-            System.out.println(lines[i] + " is NOT a palindrome.");
-        }
-    }
-}
 public boolean palindrome(String word) {
     String baseWord = onlyLetters(noCapitals(noSpaces(word))); 
     String reversedWord = reverse(baseWord);
